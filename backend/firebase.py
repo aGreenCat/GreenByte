cd2e44e5b3239fd7f6d6de47869b9b1cf6c6d08c
Nakib Abedin <nakibabedin@gmail.com>
2024-11-08 18:13:37 -0500

    add firebase helper functionality

--- backend/firebase.py
+++ backend/firebase.py
@@ -6,9 +6,30 @@ firebase_admin.initialize_app(cred, {
     "databaseURL": "https://greenbytes-5a45c-default-rtdb.firebaseio.com"  # Replace with your database URL
     })
 
+def update_counter( data , food_item ):
+    if food_item in data:
+        data[food_item] += 1
+    else:
+        data[food_item] = 1
+    return data
+
+def update_total( food_item ):
+    ref = db.reference('/total')
+    data = update_counter( ref.get(), food_item)
+    ref.set( data )
+
+def update_healthy( food_item ):
+    ref = db.reference('/healthy')
+    data = update_counter( ref.get(), food_item)
+    ref.set( data )
+
+def update_environmentally_friendly( food_item ):
+    ref = db.reference('/environmentally_friendly')
+    data = update_counter( ref.get(), food_item )
+    ref.set(data)    
+
 def main():
     ref = db.reference('/hello')
-    print( ref.get() )
 
 if __name__ == "__main__":
     main()
