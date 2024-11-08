8d63b06a9c2b595a36ea79bed573b1fcddfbc1fc
Nakib Abedin <nakibabedin@gmail.com>
2024-11-08 15:30:47 -0500

    add gemini.py

--- backend/gemini.py
+++ backend/gemini.py
@@ -1,4 +1,5 @@
 import google.generativeai as genai
+import json
 
 # Read in the API Key from the local dot_env file
 with open('keys/gemini.txt', 'r') as file:
@@ -14,13 +15,44 @@ def ask_gemini(prompt, tokens=256):
         generation_config=genai.types.GenerationConfig(
             candidate_count=1,
             max_output_tokens=tokens,
-            temperature=0.3,
+            temperature=0.1,
         ),
     )
     return response.candidates[0].content.parts[0].text
 
+# gets both nutrition data and the environmental footprint of a certain food description
+def get_food_data( food_description ):
+    prompt = f'''
+        Forget all previous context.
+        I am going to give you a description of a certain food that I am about to eat.
+        {food_description}
+
+        I want you to return something to me in the exact JSON body format that I request.
+        Your response should comprise of only the JSON body that I request and nothing other than it.
+
+        Here is the format below, and I will provide descriptions of each individual attribute in the JSON body
+        {{ 
+            "calories_lower" : int, // lower bound for calorie estimation range
+            "calories_upper" : int, // upper bound for calorie estimation range
+            "carbon_emissions": int, // the number of grams of carbon dioxide emissions
+            "gallons_per_item_produced" : int, // what would be the number of gallons of water needed to produce the food we are giving you 
+            "grams_of_protein": int,
+            "grams_of_carbs": int,
+            "grams_of_fats" : int,
+            "calories_from_protein": int,
+            "calories_from_carbs" : int,
+            "calories_from_fats" : int,
+            "healthy": boolean, // True or False depending on whether healthy or unhealthy determined by your best analysis
+            "environmentally_friendly" // True or False depenidng on whether food is environmentally friend
+        }}
+        
+    '''
+    return ask_gemini(prompt)
+    
+    
+
 def main():
-    print( ask_gemini("Write a sentence with all the characters in the alphabet") )
+    print( get_food_data("Steak") )
 
 if __name__ == "__main__":
     main()
