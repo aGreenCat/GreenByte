40c81901dc0880159093fe3738719889c5a01289
Nakib Abedin <nakibabedin@gmail.com>
2024-11-08 20:54:07 -0500

    fix logo

--- frontend/src/components/Banner.tsx
+++ frontend/src/components/Banner.tsx
@@ -10,7 +10,7 @@ interface BannerProps {
 const Banner: React.FC<BannerProps> = ({ onAboutMeClick, onLeaderboardClick}) => {
   return (
     <div className="banner">
-    <img src={clearGB} alt="GreenByte Logo" className="logo"/>
+    <img src="src/assets/normalGB.png" alt="GreenByte Logo" className="logo"/>
       <h1 className="banner-title">Welcome to Greenbyte</h1>
       <div className="button">
       <button className="about-me-button" onClick={onAboutMeClick}>
